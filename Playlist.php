0cd8a02c95bef475698e8a0e756e30f6e1da7416
Matheus Souza Reys <153141657+MatheusReys@users.noreply.github.com>
2026-05-21 20:14:22 -0300

    Playlist php criada

--- Playlist.php
+++ Playlist.php
@@ -1,2 +1,17 @@
 <?php
 
+class Playlist {
+    private array $midias = [];
+
+    
+    public function adicionar($midia): void {
+        $this->midias[] = $midia;
+    }
+
+    public function reproduzirTudo(): void {
+        foreach ($this->midias as $midia) {
+            echo $midia->reproduzir() . "<br>";
+        }
+    }
+}
+?>
